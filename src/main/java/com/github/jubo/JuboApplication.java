c61a8a2bb1fc45ed4205164726e111600182fb70
Changehee Choi <changhee.dev@gmail.com>
2020-06-16 20:58:15 +0900

    First commit

---
+++ src/main/java/com/github/jubo/JuboApplication.java
@@ -0,0 +1,13 @@
+package com.github.jubo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jubo/JuboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jubo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

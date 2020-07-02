28f41929aaa240d3203bf5599f512b9464d62489
Changehee Choi <changhee.dev@gmail.com>
2020-07-02 21:19:22 +0900

    API 호출시 액세스토큰 여부에 따라 인증 객체를 생성하는 Filter 클래스 구현

---
+++ jubo-services/manager-service/src/main/java/com/github/changhee_choi/jubo/manager/web/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,59 @@
+package com.github.changhee_choi.jubo.manager.web.filter;
+
+import com.github.changhee_choi.jubo.core.dto.UserTokenClaims;
+import com.github.changhee_choi.jubo.core.util.CookieUtil;
+import com.github.changhee_choi.jubo.core.util.JwtUtil;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * @author Changhee Choi
+ * @since 02/07/2020
+ */
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        Optional<Cookie> jwtCookie = CookieUtil.getCookie(request, "ACCESS_TOKEN");
+
+        if (jwtCookie.isPresent()) {
+            Claims claims = jwtUtil.validateToken(jwtCookie.get().getValue());
+            UserTokenClaims userTokenClaims = UserTokenClaims.builder()
+                    .id(claims.get("id", Long.class))
+                    .name(claims.get("name", String.class))
+                    .roles(claims.get("roles", List.class)).build();
+
+            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                    new UsernamePasswordAuthenticationToken(
+                            userTokenClaims,
+                            null,
+                            userTokenClaims.getRoles()
+                                    .stream()
+                                    .map(SimpleGrantedAuthority::new)
+                                    .collect(Collectors.toSet()));
+
+            //인증 정보를 SecurityContextHolder 에 저장
+            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+        }
+        filterChain.doFilter(request, response);
+    }
+}

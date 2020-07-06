582832b225232cbff79041eab8a09e82d7061139
Changehee Choi <changhee.dev@gmail.com>
2020-07-06 23:05:50 +0900

    주보 등록 및 수정 폼 데이터 매핑을 위한 Payload 클래스 구현

---
+++ jubo-services/manager-service/src/main/java/com/github/changhee_choi/jubo/manager/web/payload/JuboFormPayload.java
@@ -0,0 +1,46 @@
+package com.github.changhee_choi.jubo.manager.web.payload;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author Changhee Choi
+ * @since 06/07/2020
+ */
+@Data
+@NoArgsConstructor
+public class JuboFormPayload {
+    @NotBlank
+    @Size(min = 1, max = 50)
+    private String title;
+
+    @NotNull
+    private UUID churchId;
+
+    @NotNull
+    private LocalDateTime startDate;
+
+    @NotEmpty
+    @Valid
+    private List<JuboContentPayload> juboContents = new ArrayList<>();
+
+    @Builder
+    public JuboFormPayload(@NotBlank @Size(min = 1, max = 50) String title, @NotNull UUID churchId,
+                           @NotNull LocalDateTime startDate, @NotEmpty List<JuboContentPayload> juboContents) {
+        this.title = title;
+        this.churchId = churchId;
+        this.startDate = startDate;
+        this.juboContents = juboContents;
+    }
+}

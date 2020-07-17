e86a6170b3419bcbdc1117e13d1de0a70ae33f11
Changehee Choi <changhee.dev@gmail.com>
2020-07-17 20:14:50 +0900

    주보 컨텐츠 등록 API 컨트롤러 구현

--- jubo-services/manager-service/src/main/java/com/github/changhee_choi/jubo/manager/controller/JuboServiceController.java
+++ jubo-services/manager-service/src/main/java/com/github/changhee_choi/jubo/manager/controller/JuboServiceController.java
@@ -2,13 +2,18 @@ package com.github.changhee_choi.jubo.manager.controller;
 
 import com.github.changhee_choi.jubo.core.domain.attachment.AttachmentNotFoundException;
 import com.github.changhee_choi.jubo.core.domain.church.ChurchNotFoundException;
+import com.github.changhee_choi.jubo.core.domain.jubo.JuboContentDetails;
 import com.github.changhee_choi.jubo.core.domain.jubo.JuboDetails;
+import com.github.changhee_choi.jubo.manager.service.JuboContentService;
 import com.github.changhee_choi.jubo.manager.service.JuboService;
+import com.github.changhee_choi.jubo.manager.web.payload.ChurchManagerTokenClaims;
+import com.github.changhee_choi.jubo.manager.web.payload.JuboContentRegistrationPayload;
 import com.github.changhee_choi.jubo.manager.web.payload.JuboRegistrationPayload;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,20 +26,36 @@ import javax.validation.ValidationException;
  */
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/jubo/**")
+@RequestMapping("/jubo")
 @Slf4j
 public class JuboServiceController {
 
     private final JuboService juboService;
+    private final JuboContentService juboContentService;
 
     @PostMapping("")
-    public ResponseEntity register(@Valid @RequestBody JuboRegistrationPayload payload, BindingResult bindingResult) {
+    public ResponseEntity registerJubo(@Valid @RequestBody JuboRegistrationPayload payload, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             log.debug(bindingResult.getFieldErrors().toString());
             throw new ValidationException("주보 등록 요청 데이터가 올바르지 않습니다.");
         }
         JuboDetails juboDetails = juboService.register(payload);
         return ResponseEntity.status(HttpStatus.CREATED).body(juboDetails);
+    }
+
+    @PostMapping("/{id}/contents")
+    public ResponseEntity registerContent(@PathVariable Long id,
+                                          @Valid @RequestBody JuboContentRegistrationPayload payload,
+                                          Authentication authentication, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            log.debug(bindingResult.getFieldErrors().toString());
+            throw new ValidationException("주보 컨텐츠 등록 요청 데이터가 올바르지 않습니다.");
+        }
+
+        ChurchManagerTokenClaims tokenClaims = (ChurchManagerTokenClaims)authentication.getPrincipal();
+
+        JuboContentDetails contentDetails = juboContentService.register(tokenClaims.getChurchId(), id, payload);
+        return ResponseEntity.status(HttpStatus.CREATED).body(contentDetails);
     }
 
     @ExceptionHandler(value = {ValidationException.class})
--- jubo-services/manager-service/src/test/java/com/github/changhee_choi/jubo/manager/controller/JuboServiceControllerTests.java
+++ jubo-services/manager-service/src/test/java/com/github/changhee_choi/jubo/manager/controller/JuboServiceControllerTests.java
@@ -5,6 +5,10 @@ import com.github.changhee_choi.jubo.core.domain.attachment.AttachmentRepository
 import com.github.changhee_choi.jubo.core.domain.attachment.FileType;
 import com.github.changhee_choi.jubo.core.domain.church.Church;
 import com.github.changhee_choi.jubo.core.domain.church.ChurchRepository;
+import com.github.changhee_choi.jubo.core.domain.jubo.Jubo;
+import com.github.changhee_choi.jubo.core.domain.jubo.JuboRepository;
+import com.github.changhee_choi.jubo.manager.web.payload.ChurchManagerTokenClaims;
+import com.github.changhee_choi.jubo.manager.web.util.JwtUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +20,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+import javax.servlet.http.Cookie;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
@@ -40,7 +47,11 @@ class JuboServiceControllerTests {
     @Autowired
     private ChurchRepository churchRepository;
     @Autowired
+    private JuboRepository juboRepository;
+    @Autowired
     private AttachmentRepository attachmentRepository;
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @BeforeEach
     public void setup() {
@@ -51,9 +62,148 @@ class JuboServiceControllerTests {
                 .build();
     }
 
+    @Test
+    @WithMockUser(username = "TestUser", roles = {"CHURCH_MANAGER"})
+    void 주보등록() throws Exception {
+
+        Church church = createChurch("MyChurch", 20);
+        String payload = createJuboPayload("2020년 7월 12일 주보", church.getId().toString(), "2020-07-12 00:00:00");
+        mockMvc.perform(post("/jubo")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNotEmpty())
+                .andExpect(jsonPath("$.title").value("2020년 7월 12일 주보"))
+                .andExpect(jsonPath("$.startDate").value("2020-07-12 00:00:00"))
+                .andExpect(jsonPath("$.endDate").value("2020-07-18 00:00:00"))
+                .andExpect(jsonPath("$.viewCount").value(0));
+    }
+
+    @Test
+    @WithMockUser(username = "TestUser")
+    void 주보등록_요청을_보냈을때_유저가_Church_Manager_Role을_가지지_않은_경우_Forbidden_상태가_응답된다() throws Exception {
+        mockMvc.perform(post("/jubo")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{}"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(username = "TestUser", roles = {"CHURCH_MANAGER"})
+    void 주보등록_요청을_보냈을때_제목이_입력되지_않은경우_BadRequest_상태가_응답된다() throws Exception {
+        String payload = createJuboPayload("", UUID.randomUUID().toString(), "2020-07-12 00:00:00");
+
+        mockMvc.perform(post("/jubo")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @WithMockUser(username = "TestUser", roles = {"CHURCH_MANAGER"})
+    void 주보등록_요청을_보냈을때_교회Id가_입력되지_않은경우_BadRequest_상태가_응답된다() throws Exception {
+        String payload = createJuboPayload("2020년 7월 12일 주보", "", "2020-07-12 00:00:00");
+
+        mockMvc.perform(post("/jubo")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @WithMockUser(username = "TestUser", roles = {"CHURCH_MANAGER"})
+    void 주보등록_요청을_보냈을때_교회Id가_UUID타입이_아닌경우_BadRequest_상태가_응답된다() throws Exception {
+        String payload = createJuboPayload("2020년 7월 12일 주보", "notUUIDString", "2020-07-12 00:00:00");
+
+        mockMvc.perform(post("/jubo")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @WithMockUser(username = "TestUser", roles = {"CHURCH_MANAGER"})
+    void 주보등록_요청을_보냈을때_시작날짜가_입력되지_않은경우_BadRequest_상태가_응답된다() throws Exception {
+        String payload = createJuboPayload("2020년 7월 12일 주보", UUID.randomUUID().toString(), "");
+
+        mockMvc.perform(post("/jubo")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @WithMockUser(username = "TestUser", roles = {"CHURCH_MANAGER"})
+    void 주보등록_요청을_보냈을때_시작날짜의_형식이_yyyy_MM_dd__HH_mm_ss가_아닌경우_BadRequest_상태가_응답된다() throws Exception {
+        String payload = createJuboPayload("2020년 7월 12일 주보", UUID.randomUUID().toString(), "2020-07-12");
+
+        mockMvc.perform(post("/jubo")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void 주보_컨텐츠_등록() throws Exception {
+        //given
+        Jubo jubo = createJubo("2020년 7월 17일 주보");
+        Attachment attachment = createAttachment("testFile1.jpg", FileType.IMAGE);
+        String contentPayload = createJuboContentPayload("주일 1부예배", "내용", Arrays.asList(attachment.getId()));
+
+        //when, then
+        mockMvc.perform(post("/jubo/{id}/contents", jubo.getId())
+                .cookie(new Cookie("ACCESS_TOKEN", createAccessToken(jubo.getChurch().getId(), "ROLE_CHURCH_MANAGER")))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(contentPayload))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNotEmpty())
+                .andExpect(jsonPath("$.title").value("주일 1부예배"))
+                .andExpect(jsonPath("$.content").value("내용"))
+                .andExpect(jsonPath("$.attachments[0].id").isNotEmpty())
+                .andExpect(jsonPath("$.attachments[0].path").isNotEmpty())
+                .andExpect(jsonPath("$.attachments[0].originName").value("testFile1.jpg"))
+                .andExpect(jsonPath("$.attachments[0].fileType").value("IMAGE"));
+    }
+
+    @Test
+    void 주보_컨텐츠의_제목이_입력되지_않은_경우_BadRequest_상태가_응답된다() throws Exception {
+        //given
+        String contentPayload = createJuboContentPayload("", "내용", new ArrayList<>());
+
+        //when, then
+        mockMvc.perform(post("/jubo/{id}/contents", UUID.randomUUID())
+                .cookie(new Cookie("ACCESS_TOKEN", createAccessToken(UUID.randomUUID(), "ROLE_CHURCH_MANAGER")))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(contentPayload))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void 주보_컨텐츠의_내용이_입력되지_않은_경우_BadRequest_상태가_응답된다() throws Exception {
+        //given
+        String contentPayload = createJuboContentPayload("주일 1부 예배", "", new ArrayList<>());
+
+        //when, then
+        mockMvc.perform(post("/jubo/{id}/contents", UUID.randomUUID())
+                .cookie(new Cookie("ACCESS_TOKEN", createAccessToken(UUID.randomUUID(), "ROLE_CHURCH_MANAGER")))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(contentPayload))
+                .andExpect(status().isBadRequest());
+    }
+
     private Church createChurch(String name, int memberNum) {
         Church church = Church.builder().name(name).memberNum(memberNum).build();
         return churchRepository.save(church);
+    }
+
+    private Jubo createJubo(String title) {
+        Jubo jubo = Jubo.builder()
+                .title(title)
+                .startDate(LocalDateTime.of(2020, 7, 17, 0, 0)).build();
+
+        Church church = createChurch("MyChurch", 20);
+        jubo.setChurch(church);
+        return juboRepository.save(jubo);
     }
 
     private Attachment createAttachment(String filename, FileType fileType) {
@@ -62,7 +212,17 @@ class JuboServiceControllerTests {
         return attachmentRepository.save(attachment);
     }
 
-    private String createJuboContentPayLoad(String title, String content, List<UUID> attachmentIds) {
+    private String createJuboPayload(String title, String churchId, String startDate) {
+        StringBuilder payLoadBuilder = new StringBuilder();
+        payLoadBuilder.append("{")
+                .append("\"title\" : \"").append(title).append("\",")
+                .append("\"churchId\" : \"").append(churchId).append("\",")
+                .append("\"startDate\" : \"").append(startDate).append("\"")
+                .append("}");
+        return payLoadBuilder.toString();
+    }
+
+    private String createJuboContentPayload(String title, String content, List<UUID> attachmentIds) {
         StringBuilder payLoadBuilder = new StringBuilder();
 
         payLoadBuilder.append("{")
@@ -80,189 +240,14 @@ class JuboServiceControllerTests {
         return payLoadBuilder.toString();
     }
 
-    @Test
-    @WithMockUser(username = "TestUser", roles = {"CHURCH_MANAGER"})
-    void 주보등록() throws Exception {
+    private String createAccessToken(UUID churchId, String role) {
+        ChurchManagerTokenClaims claims = ChurchManagerTokenClaims.builder()
+                .id(1L)
+                .name("TestUser")
+                .roles(Arrays.asList(role))
+                .churchId(churchId)
+                .build();
 
-        Church church = createChurch("MyChurch", 20);
-        Attachment attachment = createAttachment("testFile1.jpg", FileType.IMAGE);
-
-        StringBuilder payLoadBuilder = new StringBuilder();
-        payLoadBuilder.append("{")
-                .append("\"title\" : \"2020년 7월 12일 주보\",")
-                .append("\"churchId\" : \"").append(church.getId()).append("\",")
-                .append("\"startDate\" : \"2020-07-12 00:00:00\",")
-                .append("\"juboContents\" : [")
-                .append(createJuboContentPayLoad("주일 1부예배", "내용", Arrays.asList(attachment.getId())))
-                .append("]}");
-
-        mockMvc.perform(post("/jubo")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(payLoadBuilder.toString()))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").isNotEmpty())
-                .andExpect(jsonPath("$.title").value("2020년 7월 12일 주보"))
-                .andExpect(jsonPath("$.startDate").value("2020-07-12 00:00:00"))
-                .andExpect(jsonPath("$.endDate").value("2020-07-18 00:00:00"))
-                .andExpect(jsonPath("$.viewCount").value(0))
-                .andExpect(jsonPath("$.contents[0].id").isNotEmpty())
-                .andExpect(jsonPath("$.contents[0].title").value("주일 1부예배"))
-                .andExpect(jsonPath("$.contents[0].content").value("내용"))
-                .andExpect(jsonPath("$.contents[0].attachments[0].id").isNotEmpty())
-                .andExpect(jsonPath("$.contents[0].attachments[0].path").isNotEmpty())
-                .andExpect(jsonPath("$.contents[0].attachments[0].originName").value("testFile1.jpg"))
-                .andExpect(jsonPath("$.contents[0].attachments[0].fileType").value("IMAGE"));
-    }
-
-    @Test
-    @WithMockUser(username = "TestUser")
-    void 주보등록_요청을_보냈을때_유저가_Church_Manager_Role을_가지지_않은_경우_Forbidden_상태가_응답된다() throws Exception {
-        mockMvc.perform(post("/jubo")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{}"))
-                .andExpect(status().isForbidden());
-    }
-
-    @Test
-    @WithMockUser(username = "TestUser", roles = {"CHURCH_MANAGER"})
-    void 주보등록_요청을_보냈을때_제목이_입력되지_않은경우_BadRequest_상태가_응답된다() throws Exception {
-        StringBuilder payLoadBuilder = new StringBuilder();
-        payLoadBuilder.append("{")
-                .append("\"title\" : \"\",")
-                .append("\"churchId\" : \"").append(UUID.randomUUID()).append("\",")
-                .append("\"startDate\" : \"2020-07-12 00:00:00\",")
-                .append("\"juboContents\" : [")
-                .append(createJuboContentPayLoad("주일 1부예배", "내용", Arrays.asList(UUID.randomUUID())))
-                .append("]}");
-
-        mockMvc.perform(post("/jubo")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(payLoadBuilder.toString()))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    @WithMockUser(username = "TestUser", roles = {"CHURCH_MANAGER"})
-    void 주보등록_요청을_보냈을때_교회Id가_입력되지_않은경우_BadRequest_상태가_응답된다() throws Exception {
-        StringBuilder payLoadBuilder = new StringBuilder();
-        payLoadBuilder.append("{")
-                .append("\"title\" : \"2020년 7월 12일 주보\",")
-                .append("\"churchId\" : \"\",")
-                .append("\"startDate\" : \"2020-07-12 00:00:00\",")
-                .append("\"juboContents\" : [")
-                .append(createJuboContentPayLoad("주일 1부예배", "내용", Arrays.asList(UUID.randomUUID())))
-                .append("]}");
-
-        mockMvc.perform(post("/jubo")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(payLoadBuilder.toString()))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    @WithMockUser(username = "TestUser", roles = {"CHURCH_MANAGER"})
-    void 주보등록_요청을_보냈을때_교회Id가_UUID타입이_아닌경우_BadRequest_상태가_응답된다() throws Exception {
-        StringBuilder payLoadBuilder = new StringBuilder();
-        payLoadBuilder.append("{")
-                .append("\"title\" : \"2020년 7월 12일 주보\",")
-                .append("\"churchId\" : \"").append("NotUUIDTypeString").append("\",")
-                .append("\"startDate\" : \"2020-07-12 00:00:00\",")
-                .append("\"juboContents\" : [")
-                .append(createJuboContentPayLoad("주일 1부예배", "내용", Arrays.asList(UUID.randomUUID())))
-                .append("]}");
-
-        mockMvc.perform(post("/jubo")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(payLoadBuilder.toString()))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    @WithMockUser(username = "TestUser", roles = {"CHURCH_MANAGER"})
-    void 주보등록_요청을_보냈을때_시작날짜가_입력되지_않은경우_BadRequest_상태가_응답된다() throws Exception {
-        StringBuilder payLoadBuilder = new StringBuilder();
-        payLoadBuilder.append("{")
-                .append("\"title\" : \"2020년 7월 12일 주보\",")
-                .append("\"churchId\" : \"").append(UUID.randomUUID()).append("\",")
-                .append("\"startDate\" : \"\",")
-                .append("\"juboContents\" : [")
-                .append(createJuboContentPayLoad("주일 1부예배", "내용", Arrays.asList(UUID.randomUUID())))
-                .append("]}");
-
-        mockMvc.perform(post("/jubo")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(payLoadBuilder.toString()))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    @WithMockUser(username = "TestUser", roles = {"CHURCH_MANAGER"})
-    void 주보등록_요청을_보냈을때_시작날짜의_형식이_yyyy_MM_dd__HH_mm_ss가_아닌경우_BadRequest_상태가_응답된다() throws Exception {
-        StringBuilder payLoadBuilder = new StringBuilder();
-        payLoadBuilder.append("{")
-                .append("\"title\" : \"2020년 7월 12일 주보\",")
-                .append("\"churchId\" : \"").append(UUID.randomUUID()).append("\",")
-                .append("\"startDate\" : \"2020-07-12\",")
-                .append("\"juboContents\" : [")
-                .append(createJuboContentPayLoad("주일 1부예배", "내용", Arrays.asList(UUID.randomUUID())))
-                .append("]}");
-
-        mockMvc.perform(post("/jubo")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(payLoadBuilder.toString()))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    @WithMockUser(username = "TestUser", roles = {"CHURCH_MANAGER"})
-    void 주보등록_요청을_보냈을때_juboContents의_사이즈가_0인_경우_BadRequest_상태가_응답된다() throws Exception {
-        StringBuilder payLoadBuilder = new StringBuilder();
-        payLoadBuilder.append("{")
-                .append("\"title\" : \"2020년 7월 12일 주보\",")
-                .append("\"churchId\" : \"").append(UUID.randomUUID()).append("\",")
-                .append("\"startDate\" : \"2020-07-12 00:00:00\",")
-                .append("\"juboContents\" : []")
-                .append("}");
-
-        mockMvc.perform(post("/jubo")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(payLoadBuilder.toString()))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    @WithMockUser(username = "TestUser", roles = {"CHURCH_MANAGER"})
-    void 주보등록_요청을_보냈을때_juboContents의_제목이_입력되지_않은_경우_BadRequest_상태가_응답된다() throws Exception {
-        StringBuilder payLoadBuilder = new StringBuilder();
-        payLoadBuilder.append("{")
-                .append("\"title\" : \"2020년 7월 12일 주보\",")
-                .append("\"churchId\" : \"").append(UUID.randomUUID()).append("\",")
-                .append("\"startDate\" : \"2020-07-12 00:00:00\",")
-                .append("\"juboContents\" : [")
-                .append(createJuboContentPayLoad("", "내용", Arrays.asList(UUID.randomUUID())))
-                .append("]}");
-
-        mockMvc.perform(post("/jubo")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(payLoadBuilder.toString()))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    @WithMockUser(username = "TestUser", roles = {"CHURCH_MANAGER"})
-    void 주보등록_요청을_보냈을때_juboContents의_내용이_입력되지_않은_경우_BadRequest_상태가_응답된다() throws Exception {
-        StringBuilder payLoadBuilder = new StringBuilder();
-        payLoadBuilder.append("{")
-                .append("\"title\" : \"2020년 7월 12일 주보\",")
-                .append("\"churchId\" : \"").append(UUID.randomUUID()).append("\",")
-                .append("\"startDate\" : \"2020-07-12 00:00:00\",")
-                .append("\"juboContents\" : [")
-                .append(createJuboContentPayLoad("주일 1부예배", "", Arrays.asList(UUID.randomUUID())))
-                .append("]}");
-
-        mockMvc.perform(post("/jubo")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(payLoadBuilder.toString()))
-                .andExpect(status().isBadRequest());
+        return jwtUtil.generateToken(claims);
     }
 }

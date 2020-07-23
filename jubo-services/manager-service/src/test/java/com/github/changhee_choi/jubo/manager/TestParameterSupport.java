ab2e07703b14208ce786cefca6ffd6f416c3ef29
Changehee Choi <changhee.dev@gmail.com>
2020-07-23 11:12:22 +0900

    테스트에서 공통으로 사용되는 코드, 인스턴스들을 관리하는 클래스 추가

---
+++ jubo-services/manager-service/src/test/java/com/github/changhee_choi/jubo/manager/TestParameterSupport.java
@@ -0,0 +1,15 @@
+package com.github.changhee_choi.jubo.manager;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author Changhee Choi
+ * @since 23/07/2020
+ */
+public class TestParameterSupport {
+    protected final UUID churchId = UUID.fromString("dbe07414-49d1-11e6-b7a7-0242ac140002");
+    protected final Long juboId = 1L;
+    protected final List<UUID> attachmentIds = Arrays.asList(UUID.fromString("a0fd7051-c82e-11ea-a901-0242ac120003"));
+}
